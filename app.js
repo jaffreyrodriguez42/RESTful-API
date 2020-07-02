11e109e195b0868d8072c77eba87eeeaaea1ca99
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-02 15:53:38 +0800

    Chained Route Handlers Using Express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// we use postman here to do all the requests
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,45 +21,43 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route("articles")
 
-//TODO
-
-app.get("/articles", function (req, res) {
-    Article.find(function (err, foundArticles) {
-        // console.log(foundArticles);
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            // console.log(foundArticles);
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        })
     })
-});
 
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article");
+            } else {
+                res.send(err);
+            }
+        });
+    })
 
-app.post("/articles", function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles");
+            } else {
+                res.send(err);
+            }
+        });
     });
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Successfully added a new article");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
-app.delete("/articles", function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Successfully deleted all articles");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
