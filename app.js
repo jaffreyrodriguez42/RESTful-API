51d680bd68933dd4e87c0206c63128dcea753ccc
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-02 21:57:58 +0800

    PUT a specific article

--- app.js
+++ app.js
@@ -71,6 +71,19 @@ app.route("/articles/:articleTitle")
                 res.send("No articles matching that title was found.");
             }
         });
+    })
+
+    .put(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+            { overwrite: true },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article.");
+                }
+            }
+        );
     });
 
 
