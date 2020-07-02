5ddb290eff88c9c150509b635179cd74711704c7
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-02 22:12:11 +0800

    Patch a specific article

--- app.js
+++ app.js
@@ -84,7 +84,23 @@ app.route("/articles/:articleTitle")
                 }
             }
         );
+    })
+
+    .patch(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
     });
+
+
 
 
 
